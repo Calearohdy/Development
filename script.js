a4463efac475a47486dc75d25776821cd1b37468
Caleb Rohdy <Calearohdy@gmail.com>
2017-05-04 00:06:21 -0400

    modified:   Purrfect.html 	new file:   jsDemo.html

---
+++ script.js
@@ -0,0 +1,7 @@
+//var firstName = prompt("What is your first name?");
+//var lastName = prompt(firstName + ", what is your last name?");
+var age = prompt("How old are you?");
+var total = age * 365.25;
+alert("You have lived for " + total + " days");
+
+console.log(total + " days")
