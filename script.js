2a111d643cc212ce3962e7da5f97b82b0c130efa
Caleb Rohdy <Calearohdy@gmail.com>
2017-05-07 23:25:03 -0400

    Exercise #2

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 //var firstName = prompt("What is your first name?");
 //var lastName = prompt(firstName + ", what is your last name?");
-var age = prompt("How old are you?");
-var total = age * 365.25;
-alert("You have lived for " + total + " days");
 
-console.log(total + " days")
+var age = prompt("How old are you?");
+alert(concertB(age));
+
+function concertB(age) {
+
+var answer = "";
+
+if (age < 18) {
+answer = "You are too young to enter";
+} else if (age < 21) {
+answer = "You can enter, but no drinking allowed";
+} else
+answer = "You can enter and drink!";
+
+
+ return answer;
+}
