e5df406a95a52f141b7532eb27227bfebd845149
Caleb Rohdy <Calearohdy@gmail.com>
2017-05-23 23:25:02 -0400

    Arrays updated

--- script.js
+++ script.js
@@ -1,16 +1,141 @@
+// Arrays
+	var sum = [1,2,5];
+	var count = 0;
+
+	function sumArray(sum){
+		for (var i = 1; i < sum.length; i++) {
+			count += sum[i];
+		}
+		return count;
+	}
+	console.log(sumArray(sum));
+/*
+var same = ["a","a","b"];
+
+	function isUniform(same){
+		for (var i = 1; i < same.length; i++) {
+			if (same[i] !== same[0]) 
+				return false;
+		}
+		return true;
+	}
+	console.log(isUniform(same));
+
+var order = ["a","b","c"];
+	function printReverse(order){
+		var rev = new Array;
+		for (var i = order.length - 1; i >= 0; i--) {
+			rev.push(order[i]);
+	}
+	return rev;
+}
+	console.log(printReverse(order));
+
+var todo = ["Buy laptop"];
+
+var input = prompt("What would you like to do?")
+
+while(input !== "quit"){
+
+	if (input === "list") {
+		console.log("*********");
+		todo.forEach(function(todo, i){	
+		console.log(i + ": " + todo);
+	});
+		console.log("*********");
+	} else if (input === "new") {
+		var newTodo = prompt("Enter new todo");
+		todo.push(newTodo);
+		console.log("Added.")
+	} else if (input === "delete") {
+		var index = prompt("Enter index of todo to delete");
+
+		todo.splice(index,1);
+		console.log("Deleted.")
+	}
+
+	input = prompt("What would you like to do?");
+}
+console.log("Session Ended");
+
+//Functions
+
+var strKebab = "----";
+console.log(kebabToSnake(strKebab));
+
+function kebabToSnake(strKebab){
+
+	var strSnake = strKebab.replace(/-/g , "_");
+
+		return strSnake;
+}
+		if (text === "-") {
+			text.replace("-","_");
+		}
+		var strSnake = text;
+
+var answer = prompt("Guess a number");
+
+alert(factorial(answer));
+
+function factorial (answer){
+	if (answer === 0) {
+		return 1;
+	}
+
+	return answer * factorial(answer - 1);
+}
+
+var number = (prompt("Guess a number"));
+//alert(isEven(3));
+console.log(isEven(number));
+
+function isEven(){
+	var answer = "";
+
+	if (Number(number) % 2 === 0) {
+	 	answer = "The number " + number + " is even";
+	} else {
+		answer = "The number " + number + " is odd";
+	}
+		return answer;
+}
+
+
+//for loops
+//for (var i = -10; i <= 20; i++) {
+//	console.log(i)
+//}
+
+//for (var i = 10; i <= 40; i+= 2) {
+//	console.log(i)
+//}
+
+
+for (var i = 300; i <= 333; i++) {
+	if (i % 2 !== 0) {
+		console.log(i)
+	}
+}
+
+for (var i = 5; i <= 50; i++) {
+	if (i % 5 === 0 && i % 3 === 0) {
+		console.log(i)
+	}
+}
+
 //Using While loops
 
 var annoy = prompt("Are we there yet?");
 
-while(annoy !== "yes"){
+while(annoy.indexOf("yes") === -1){
 	prompt("Are we there yet?");
-	if (annoy === "yes") {
-		alert("About time");
-		console.log("About Time");
-	}
 }
 
-/*
+		alert("About time");
+		console.log("About Time");
+
+
 var loops = -10;
 
 while (loops < 20){
