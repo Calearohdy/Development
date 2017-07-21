2b09e313f8615314057e2ca95a775046f705d2c5
Cale Rohdy <calerohdy@Cales-MacBook-Pro.local>
2017-07-21 07:58:21 -0400

    safety commit

--- scripts/todojs.js
+++ scripts/todojs.js
@@ -1,13 +1,24 @@
 //Check off Todos 
 //if li is gray turn black else turn gray
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 //click on X to delete
-$("span").click(function() {
+$("ul").on("click", "span", function() {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
+})
+
+$("input[type='text").keypress(function(event) {
+    if (event.which === 13) {
+        var todoText = ($(this).val()); //grabbing new text from input
+        $(this).val("");
+        //create a new li
+        $("ul").append("<li><span>x</span> " + todoText + "</li>")
+    }
+
+    //console.log("key pressed");
 })
