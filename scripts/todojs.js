248aaf1d622cec24deb6371deea8a776e4515794
Cale Rohdy <calerohdy@Cales-MacBook-Pro.local>
2017-07-22 09:29:07 -0400

    project TodoList complete

--- scripts/todojs.js
+++ scripts/todojs.js
@@ -17,8 +17,10 @@ $("input[type='text").keypress(function(event) {
         var todoText = ($(this).val()); //grabbing new text from input
         $(this).val("");
         //create a new li
-        $("ul").append("<li><span>x</span> " + todoText + "</li>")
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
     }
+})
 
-    //console.log("key pressed");
+$(".fa-plus").click(function() {
+    $("input[type='text").fadeToggle("faster", "linear");
 })
